97007385b99ad4c2be6498246697347f0a7d0cd7
Dave Rice <dave@dericed.com>
2016-10-28 15:48:30 -0400

    fix broken link to stat report

--- QueryResourceSpace.sh
+++ QueryResourceSpace.sh
@@ -15,10 +15,10 @@ if [ -d "${1}" ] ; then
     find "${1}" -type f -mindepth 1 -maxdepth 1 > /tmp/querylist.txt
 elif [ -f "${HOME}/Desktop/REPORTS/${REPORTDATE}/omneon/what_is_on_the_omneon_ids_only_cunytv_only.txt" ] ; then
     IDLIST="${HOME}/Desktop/REPORTS/${REPORTDATE}/omneon/what_is_on_the_omneon_ids_only_cunytv_only.txt"
-    STATLIST="${HOME}/Desktop/REPORTS/${REPORTDATE}/omneon/what_is_on_the_omneon_stat.txt"
+    STATLIST="${HOME}/Desktop/REPORTS/${REPORTDATE}/internal/what_is_on_the_omneon_stat.txt"
 elif [ -f "/Volumes/archivesx/Desktop/REPORTS/${REPORTDATE}/omneon/what_is_on_the_omneon_ids_only_cunytv_only.txt" ] ; then
     IDLIST="/Volumes/archivesx/Desktop/REPORTS/${REPORTDATE}/omneon/what_is_on_the_omneon_ids_only_cunytv_only.txt"
-    STATLIST="/Volumes/archivesx/Desktop/REPORTS/${REPORTDATE}/omneon/what_is_on_the_omneon_stat.txt"
+    STATLIST="/Volumes/archivesx/Desktop/REPORTS/${REPORTDATE}/internal/what_is_on_the_omneon_stat.txt"
 elif [ ! -f "${IDLIST}" ] ; then
     _report -wt "Error: I'm not able to load and read the list of IDs on the omneon!"
     exit 1
