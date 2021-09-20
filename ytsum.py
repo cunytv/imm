5864f49505658f164764465681ded1f18078e7f3
CSchloss385 <catriona.schlosser@tv.cuny.edu>
2021-09-20 16:57:27 -0400

    added try/except lines and added to csv

--- ytsum.py
+++ ytsum.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+"""
+    ystum.py outputs a csv file on your desktop with metadata about youtube uploads using youtubeids 
+    from a csv input. 
+    Last Revised: 9/20/2021
+"""
 
 import subprocess  
 import csv
 #import youtube_dl
 import sys
 import os
-#import datetime
 import time
 
-
-reader = csv.reader(open(sys.argv[1]))
+try: 
+    reader = csv.reader(open(sys.argv[1]))
+except IndexError:
+    print(f'usage: ytsum.py [input.csv]')
+    exit()
 
 next(reader)
 
@@ -26,7 +33,7 @@ print(f'Your Youtube summary report can be found here: {filename}')
 print()
 with open(filename, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
-    writer.writerow(['MediaID', 'YouTubeID', 'Extension', 'Size', 'Resolution', 'Frame Rate', 'Captions'])
+    writer.writerow(['MediaID', 'YouTubeID', 'Extension', 'Size', 'Resolution', 'Frame Rate', 'Captions', 'Title'])
 
 for line in reader:
     
@@ -34,26 +41,36 @@ for line in reader:
 
     mediaid=(line[0])
     
+    title=(line[2])
+    
     if youtubeid == '':
         with open(filename, 'a', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            rows2 = [mediaid, youtubeid]
+            rows2 = [mediaid, youtubeid, title]
             writer.writerow(rows2) 
         continue
     
-    videoinfo = subprocess.check_output(['youtube-dl', '--list-formats', '--skip-download', youtubeid])
+    try:
+        videoinfo = subprocess.check_output(['youtube-dl', '--list-formats', '--skip-download','--', youtubeid])
     
-    videoinfosplit = videoinfo.splitlines()
-    videolastline = (videoinfosplit[-1])
-    videolastline = videolastline.decode()
+        videoinfosplit = videoinfo.splitlines()
+        videolastline = (videoinfosplit[-1])
+        videolastline = videolastline.decode()
     
-    videosep = videolastline.split()
-    filetype = videosep[1]
-    size = videosep[2]
-    resolution = videosep[3]
-    fps = videosep[7]
+        videosep = videolastline.split()
+        filetype = videosep[1]
+        size = videosep[2]
+        resolution = videosep[3]
+        fps = videosep[7]
+    except subprocess.CalledProcessError:
+        with open(filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=',')
+            rows2 = [mediaid, youtubeid]
+            writer.writerow(rows2)
+        print(f'{youtubeid} is unavailable')
+        continue
     
-    subtitles = subprocess.check_output(['youtube-dl', '--skip-download', '--list-subs', youtubeid])
+    subtitles = subprocess.check_output(['youtube-dl', '--skip-download', '--list-subs','--', youtubeid])
 
     splitsub = subtitles.splitlines()
     subtitles = (splitsub[-1])
@@ -62,7 +79,7 @@ for line in reader:
     if subtitles.startswith(youtubeid):
         subtitles = ''
         
-    rows = [mediaid, youtubeid, filetype, size, resolution, fps, subtitles]
+    rows = [mediaid, youtubeid, filetype, size, resolution, fps, subtitles, title]
 
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
