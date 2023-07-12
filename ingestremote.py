2841ffbaa2a04272fa6f52360a4199c199a7427f
CSchloss385 <catriona.schlosser@tv.cuny.edu>
2023-07-12 17:33:33 -0400

    Update ingestremote.py
    Reorganized and added ssd workflow

--- ingestremote.py
+++ ingestremote.py
@@ -2,7 +2,7 @@
 
 """
     ingestremote.py moves files from a camera card and organizes them into a package. 
-    Last Revised: 2023-06-21
+    Last Revised: 2023-07-12
 """
 
 import sys
@@ -10,156 +10,181 @@ import os
 import subprocess
 import shutil 
 
-cameradirectory = "/Volumes/Untitled"
+package_name = input("Enter a package name for this material: ")
 
-# identify camera type
+outputdirectory = "/Volumes/CUNYTV_Media/archive_projects/sxs_ingests-unique"
+
+if not os.path.exists(outputdirectory):
+	print(f'you might not be connected to the server. Reconnect and try again')
+
+package_destination = os.path.join(outputdirectory, package_name)
+
+os.makedirs(package_destination)
+
+cameradirectory = "/Volumes/"
 
 for cameratype in os.listdir(cameradirectory):
 
-	# identify workflow
+	searchstring_untitled = "Untitled"
 
-	searchstring_xdroot = 'XDROOT'
+	if searchstring_untitled in cameratype:
 
-	searchstring_BPAV = 'BPAV'
+		cameracardinput = os.path.join(cameradirectory, cameratype)
 
-	# XQD Workflow
+		# identify camera card type
 
-	if searchstring_xdroot in cameratype:
+		for cameracard_id in os.listdir(cameracardinput):
 
-		print(f'this is a XAVC (SONY)')
+			searchstring_xdroot = 'XDROOT'
+			searchstring_BPAV = 'BPAV'
 
-		inputdirectory = "/Volumes/Untitled/"
+			# XQD workflow
 
-		package_name = input("Enter a package name for this material: ")
+			if searchstring_xdroot in cameracard_id:
+				print(f'This is an XAVC. These files will rsync to the sxs_ingests-unique folder. Logs will be moved to metadata folder')
+			
+				sourcedirectory = "/Volumes/Untitled/"
 
-		outputdirectory = "/Volumes/CUNYTV_Media/archive_projects/sxs_ingests-unique/ingestremotetest"
-		
-		package_destination = os.path.join(outputdirectory, package_name)
+				camera_card_number = ''
 
-		os.makedirs(package_destination)
+				while camera_card_number != 'quit':
 
-		
-		# Enter card number and transfer files
+					camera_card_number = input("Enter the card number. If finished ingesting, type 'quit': ")
 
-		camera_card_number = ''
+					if camera_card_number != 'quit':
 
-		while camera_card_number != 'quit':
+						camera_card_number_path = os.path.join(package_destination, camera_card_number)
 
-			camera_card_number = input("Enter the card number. If finished ingesting, tyep 'quit': ")
+						os.makedirs(camera_card_number_path)
 
-			if camera_card_number != 'quit':
+						subprocess.call(["rsync", "-rtvP", sourcedirectory, camera_card_number_path])
 
-				camera_card_number_path = os.path.join(package_destination, camera_card_number)
+						# create an objects directory and move files into folder		
 
-				os.makedirs(camera_card_number_path)
+						objectspath = os.path.join(package_destination, "objects")
 
-				subprocess.call(["rsync", "-rtvP", inputdirectory, camera_card_number_path])
+						if not os.path.exists(objectspath):
+							os.mkdir(objectspath)
 
-				
-				# create an objects directory and move files into folder		
-
-				objectspath = os.path.join(package_destination, "objects")
-				print(f'this is the {objectspath}')
-
-				if not os.path.exists(objectspath):
-					os.mkdir(objectspath)
-
-				if os.path.exists(objectspath) and os.path.exists(camera_card_number_path):
-					shutil.move(camera_card_number_path, objectspath)
+						if os.path.exists(objectspath) and os.path.exists(camera_card_number_path):
+							shutil.move(camera_card_number_path, objectspath)
 
 				
-				# create metadata directory and transfer logs
+ 						# create metadata directory and transfer logs
 
-				metadata_directory = os.path.join(package_destination, "metadata")
+						metadata_directory = os.path.join(package_destination, "metadata")
 
-				if not os.path.exists(metadata_directory):
-					os.mkdir(metadata_directory)
+						if not os.path.exists(metadata_directory):
+ 							os.mkdir(metadata_directory)
 
-				camera_log_path = os.path.join(metadata_directory, camera_card_number)
+						camera_log_path = os.path.join(metadata_directory, camera_card_number)
 
-				if not os.path.exists(camera_log_path):
-					os.makedirs(camera_log_path)
 
-				camera_log_path_xdroot = os.path.join(camera_log_path, "logs/camera/XDROOT")
+						if not os.path.exists(camera_log_path):
+ 							os.makedirs(camera_log_path)
 
-				camera_log_path_clip = os.path.join(camera_log_path, "logs/camera/XDROOT/Clip")
+						camera_log_path_xdroot = os.path.join(camera_log_path, "logs/camera/XDROOT")
 
-				if not os.path.exists(camera_log_path_clip):
-					os.makedirs(camera_log_path_clip)
+						camera_log_path_clip = os.path.join(camera_log_path, "logs/camera/XDROOT/Clip")
+
+						if not os.path.exists(camera_log_path_clip):
+ 							os.makedirs(camera_log_path_clip)
 					
-				for root, dirs, files, in os.walk(objectspath):
+						for root, dirs, files, in os.walk(objectspath):
 
-					for name in files:
-						file_name = os.path.join(root,name)
+							for name in files:
+ 								file_name = os.path.join(root,name)
 
-						searchstring_cueup = 'CUEUP.XML'
-						searchstring_discmeta = 'DISCMETA.XML'
-						searchstring_mediapro = 'MEDIAPRO.XML'
+ 								searchstring_cueup = 'CUEUP.XML'
+ 								searchstring_discmeta = 'DISCMETA.XML'
+ 								searchstring_mediapro = 'MEDIAPRO.XML'
 
-						if searchstring_cueup in file_name:
-							try:
-								shutil.move(file_name, camera_log_path_xdroot)
-							except shutil.Error:
-								continue
+ 								if searchstring_cueup in file_name:
+ 									try:
+ 										shutil.move(file_name, camera_log_path_xdroot)
+ 									except shutil.Error:
+ 										continue
 
-						if searchstring_discmeta in file_name:
-							try: 
-								shutil.move(file_name, camera_log_path_xdroot)
-							except shutil.Error:
-								continue
+ 								if searchstring_discmeta in file_name:
+ 									try: 
+ 										shutil.move(file_name, camera_log_path_xdroot)
+ 									except shutil.Error:
+ 										continue
 
-						if searchstring_mediapro in file_name:
-							try:
-								shutil.move(file_name, camera_log_path_xdroot)
-							except	shutil.Error:
-								continue
+ 								if searchstring_mediapro in file_name:
+ 									try:
+ 										shutil.move(file_name, camera_log_path_xdroot)
+ 									except	shutil.Error:
+ 										continue
 
-						if file_name.endswith('.BIM'):
-							try:
-								shutil.move(file_name, camera_log_path_clip)
-							except shutil.Error:
-								continue
+ 								if file_name.endswith('.BIM'):
+ 									try:
+ 										shutil.move(file_name, camera_log_path_clip)
+ 									except shutil.Error:
+ 										continue
 
-						searchstring_clip = 'Clip'
+ 								searchstring_clip = 'Clip'
 
-						if searchstring_clip in file_name:
-							if file_name.endswith('.XML'):
-								try:
-									shutil.move(file_name, camera_log_path_clip)
-								except shutil.Error:
-									continue	
+ 								if searchstring_clip in file_name:
+ 									if file_name.endswith('.XML'):
+ 										try:
+ 											shutil.move(file_name, camera_log_path_clip)
+ 										except shutil.Error:
+ 											continue	
 
 
-				# delete empty folders
+ 						# delete empty folders
 
-				for root, dirs, files in os.walk(package_destination, topdown=False):
+						for root, dirs, files in os.walk(package_destination, topdown=False):
 					
-				 	for empty in dirs:
-				  		if len(os.listdir(os.path.join(root, empty))) == 0:
-				  			os.rmdir(os.path.join(root, empty))
-				  		else: 
-				  			print(f'{(os.path.join(root,empty))} is not empty. These will not be removed')
+ 				 			for empty in dirs:
+ 				  				if len(os.listdir(os.path.join(root, empty))) == 0:
+ 				  					os.rmdir(os.path.join(root, empty))
+ 				  				else: 
+ 				  					print(f'{(os.path.join(root,empty))} is not empty. These will not be removed')
 
-	# BPAV workflow to be expanded
 
-	# elif searchstring_BPAV in cameratype:
-		
-	# 	print(f'this is SONY XDCAM BPAV. These will have to be transcoded')
+			#BPAV workflow to expand upon.  				  					
+			
+			if searchstring_BPAV in cameracard_id:
+				print(f'This is SONY XDCAM BPAV. These files will rsync to the sxs_ingests-unique and will be rewrapped')
 
-	# 	inputdirectory = "/Volumes/Untitled/"
+	# SSD workflow
 
-	# 	package_name = input("Enter a package name for this material: ")
+	else: 
+		searchstring_ssd = "DiskHFS"
 
-	# 	outputdirectory = "/Volumes/CUNYTV_Media/archive_projects/sxs_ingests-unique/ingestremotetest"
-		
-	# 	package_destination = os.path.join(outputdirectory, package_name)
+		if searchstring_ssd in cameratype:
+			print('This material is from an SSD. Will rsync to the sxs_ingests-unique folder')
 
-	# 	os.makedirs(package_destination)
+			inputdirectory = "/Volumes/DiskHFS/"
 
-	# 	camera_card_number = input("Enter the card number. If finished ingesting press control C: ")
+			camera_card_number = ''
 
-	# 	camera_card_number_path = os.path.join(package_destination, camera_card_number)
+			while camera_card_number != 'quit':
 
-	# 	os.makedirs(camera_card_number_path)
+				camera_card_number = input("Enter the card number. If finished ingesting, type 'quit': ")
+	
+				if camera_card_number != 'quit':
 
-	# 	subprocess.call(["rsync", "-rtvP", inputdirectory, camera_card_number_path])
+ 					camera_card_number_path = os.path.join(package_destination, camera_card_number)
+
+ 					os.makedirs(camera_card_number_path)
+
+ 					for items in os.listdir(inputdirectory):
+
+ 						itemstomove = os.path.join(inputdirectory, items)
+ 						print(f'this is what i want to move {itemstomove}')
+
+ 						subprocess.call(["rsync", "-rtvP", itemstomove, camera_card_number_path])
+
+ 					#create an objects directory and move files into folder		
+
+ 					objectspath = os.path.join(package_destination, "objects")
+ 					print(f'this is the {objectspath}')
+
+ 					if not os.path.exists(objectspath):
+ 						os.mkdir(objectspath)
+
+ 					if os.path.exists(objectspath) and os.path.exists(camera_card_number_path):
+ 						shutil.move(camera_card_number_path, objectspath)
