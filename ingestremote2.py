fde46196ec414f38460023d0459094c8bd679ad3
agarrkoch <85997789+agarrkoch@users.noreply.github.com>
2024-03-20 12:07:26 -0400

    updated ingestremote2

--- ingestremote2.py
+++ ingestremote2.py
@@ -2,39 +2,38 @@ import time
 import sys
 import os
 import hashlib
-
+import detectrecentlyinserteddrives
 import subprocess
 import shutil
 
-#next list all files in directory, populate dictionary, do checksum, move, then checksum again
-#shutil.copy2(source_file, destination_folder)
+# next list all files in directory, populate dictionary, do checksum, move, then checksum again
+# shutil.copy2(source_file, destination_folder)
 
 cards_dict = {}
 server = "/Volumes/CUNYTV_Media/archive_projects/sxs_ingests-unique"
 
-objects_v = ['mxf', 'mp4', 'avi', 'mkv', 'wmv', 'mov', 'flv', 'webm', '3gp', 'mpeg', 'vob', 'rm', 'ogv', 'asf', 'mpg', 'm4v']
+objects_v = ['mxf', 'mp4', 'avi', 'mkv', 'wmv', 'mov', 'flv', 'webm', '3gp', 'mpeg', 'vob', 'rm', 'ogv', 'asf', 'mpg',
+             'm4v']
 objects_a = ['mp3', 'wav', 'aac', 'ogg', 'flac', 'm4a', 'wma', 'aiff', 'ape', 'ac3', 'amr', 'au', 'midi', 'mp2', 'ra']
 objects_i = ['jpeg', 'png', 'gif', 'bmp', 'tiff', 'svg', 'webp', 'ico', 'jpg', 'tga', 'psd', 'pdf', 'eps', 'ai', 'raw']
 metadata = ['exif', 'xmp', 'iptc', 'json', 'xml', 'csv', 'yaml', 'bim', 'md', 'log', 'txt', 'ppn', 'smi', 'motn']
 
-checks0m = False
+do_checksum = False
+do_commands = False
 failed_checks0m_f = []
 failed_checks0m_c = []
 
-def list_drives(d='/Volumes'):
-    drives = [drive for drive in os.listdir(d) if os.path.ismount(os.path.join(d, drive))]
-    return drives
+
+def server_check():
+    if not os.path.exists(server):
+        print(f'You might not be connected to the server. Reconnect and try again')
+        sys.exit(1)
 
 def countdown(s):
     for i in range(s, 0, -1):
         print(f"{i}...", end='', flush=True)
         time.sleep(1)
     print()
-
-def server_check():
-    if not os.path.exists(server):
-        print(f'You might not be connected to the server. Reconnect and try again')
-        sys.exit(1)
 
 def directory_create(p, c):
     os.makedirs(server + f'/{p}', exist_ok=True)
@@ -74,6 +73,7 @@ def file_type(f):
     else:
         return ('')
 
+
 def transfer(d='/Volumes'):
     for c in cards_dict:
         path = os.path.join(d, c)
@@ -84,13 +84,13 @@ def transfer(d='/Volumes'):
                     print(f'Transferring file {f}')
                     fp = os.path.join(foldername, f)
 
-                    if checks0m:
+                    if do_checksum:
                         cs1 = calculate_sha256_checksum(fp)
 
                     d = os.path.join(server, cards_dict[c][0], file_type(f), cards_dict[c][1])
                     shutil.copy2(fp, d)
 
-                    if checks0m:
+                    if do_checksum:
                         fp2 = os.path.join(d, f)
                         cs2 = calculate_sha256_checksum(fp2)
 
@@ -103,11 +103,16 @@ def transfer(d='/Volumes'):
                             failed_checks0m_f.append(fp2)
                             failed_checks0m_c.append(cards_dict[c][0])
 
+
 def eject(d='/Volumes'):
     print()
     for c in cards_dict:
         path = os.path.join(d, c)
         subprocess.run(["diskutil", "eject", path])
+
+    message = f"Content transferred. Safe to eject all card(s)"
+    notification(message)
+
 
 def notification(message):
     applescript = f'''
@@ -115,8 +120,8 @@ def notification(message):
         '''
     subprocess.run(["osascript", "-e", applescript])
 
-def commands():
 
+def commands():
     for card in cards_dict:
         if cards_dict[card][0] not in failed_checks0m_c:
 
@@ -175,34 +180,42 @@ def commands():
 
 
 if __name__ == "__main__":
+    # Check if connected to server
     server_check()
 
+    # Continue processing camera cards until user types 'q' for quit
     cont = ''
-    input("Remove all camera cards. Press enter to continue. ")
-    volumes = list_drives()
+
+    # Prompt user
+    input("Please insert camera card(s). Press enter to continue. ")
 
     while cont != 'q':
-        input("Insert camera cards. Press enter to continue. ")
-        i1 = input("Fixity check before and after transfer? y/n: ")
-        i2 = input("Run makeyoutube, makemetdata, checksumpackage? y/n: ")
-        if i1 == 'y':
-            checks0m = True
-
+        # Buffer to give sufficient time to mount drive
         countdown(5)
 
-        volumes2 = list_drives()
-        volumesnew = []
-
-        for drive in volumes2:
-            if drive not in volumes:
-                volumesnew.append(drive)
+        # Detect inserted drive
+        volumesnew = DetectRecentlyInsertedDrives.volume_paths()
 
         if volumesnew:
-            print(f"{len(volumesnew)} card(s) detected:")
+            print(f"{len(volumesnew)} card(s) detected")
+
             for card in volumesnew:
                 print(f"- {card}")
-                package_name = input(f"     Enter a package name: ")
-                camera_card_number = input(f"     Enter the corresponding card number on sticker (serialize from 1 if none): ")
+                i1 = (input("\tFixity check before and after transfer? y/n: ")).lower()
+                i2 = (input("\tRun makeyoutube, makemetdata, checksumpackage? y/n: ")).lower()
+
+                if i1 == 'y':
+                    do_checksum = True
+                else:
+                    do_checksum = False
+
+                if i2 == 'y':
+                    do_commands = True
+                else:
+                    do_commands = False
+
+                package_name = input(f"\tEnter a package name: ")
+                camera_card_number = input(f"\tEnter the corresponding card number on sticker (serialize from 1 if none): ")
                 camera_card_number = camera_card_number.replace(" ", "_")
 
                 files_dict = {}
@@ -211,8 +224,6 @@ if __name__ == "__main__":
 
             transfer()
             eject()
-            message = f"Content transferred. Safe to eject all card(s)"
-            notification(message)
 
             if i2 == 'y':
                 commands()
@@ -220,5 +231,5 @@ if __name__ == "__main__":
         else:
             print("No cards detected")
 
-        cont = input("Press enter to continue or q to quit. ")
+        cont = input("Insert another card and press enter to continue or q to quit. ")
         cards = {}
