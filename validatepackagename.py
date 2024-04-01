1d58da4ec430a2c35e22e1e83c797243173bc778
agarrkoch <85997789+agarrkoch@users.noreply.github.com>
2024-04-01 14:12:02 -0400

    Update validatepackagename.py

--- validatepackagename.py
+++ validatepackagename.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import re
+import os
 
-# Validates package name by confirming show code and date pattern, capitalizing all letters, deleting spaces
-def package(name):
+# Validates camera card package name by confirming show code and date pattern, capitalizing all letters, deleting spaces
+def card_package_name(name):
     # Checks pattern
     pattern = r'^[A-Za-z]{3,4}\d{4}(0[1-9]|1[0-2])(0[1-9]|[1-2]\d|3[0-1])$'
     if not re.match(pattern, name.split('_', 1)[0]):
@@ -19,11 +20,42 @@ def package(name):
     name = name.upper()
     return name
 
-# Validates subfolder name by replacing spaces with underscores, capitalizing all letters
-def subfolder(name):
+# Validates camera card subfolder name by replacing spaces with underscores, capitalizing all letters
+def card_subfolder_name(name):
     while not name:
         new_name = input(f"Subfolder name cannot be blank. Please type new name: ")
         name = new_name
+
+    # Eliminates spaces and capitalizes letters
     name = name.replace(" ", "")
     name = name.upper()
     return name
+
+# Searches string for valid emails and returns an array of email(s)
+def emails(string):
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    # Texts in string that match email pattern
+    emails = re.findall(email_pattern, string)
+    # Texts in string that do not match email pattern
+    non_matching_parts = re.split(email_pattern, string)
+    # Filter out empty strings (which are the matches) and strip any leading/trailing whitespace
+    non_matching_text = [part.strip() for part in non_matching_parts if part.strip()]
+
+    while non_matching_text:
+        print ("Invalid emails detected.")
+        print (f"\tValid: {emails}")
+        print (f"\tInvalid: {non_matching_text}")
+        string = input("Re-enter incorrect inputs: ")
+
+        emails.extend(re.findall(email_pattern, string))
+        non_matching_parts = re.split(email_pattern, string)
+        non_matching_text = [part.strip() for part in non_matching_parts if part.strip()]
+
+    return emails
+
+# Checks if path exists
+def path(path):
+    while os.path.exists(path) == False:
+        path = input("Invalid path. Please reenter: ")
+    return (path)
