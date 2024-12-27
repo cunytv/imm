041fcc904caa9cad828919469e6a839952d6b48f
agarrkoch <85997789+agarrkoch@users.noreply.github.com>
2024-12-27 11:56:20 -0500

    Update longpoll.py

--- longpoll.py
+++ longpoll.py
@@ -3,11 +3,14 @@ import requests
 import json
 import time
 import os
+import sendnetworkmail
+import sys
+import atexit
+
 
 class LongPoll:
 
     def __init__(self):
-
         # Library of image extensions
         self.image_extensions = [
             "jpg", "jpeg", "png", "gif", "bmp", "tiff", "tif", "webp", "svg", "heif", "heic", "ico",
@@ -18,10 +21,17 @@ class LongPoll:
         ]
 
         # Credentials for creating access token
+        ## AG's personal dropbox
         self.client_id = 'bsp8x2pbkklqbz8'
         self.client_secret = 'c3po7io03u5zgtt'
         self.refresh_token = 'diOhyTjXTgsAAAAAAAAAAfak8rrGSeI0tELBy1SdQceJyvoei6qBfsSXFvAMOzio'
         self.ACCESS_TOKEN = ''
+
+        ## CS's personal dropbox
+        #self.client_id = 'wjmmemxgpuxh911'
+        #self.client_secret = 'mynnf0nelu4xahk'
+        #self.refresh_token = 'ST-MxmX3A50AAAAAAAAAAahnN5Tez_DKUHRTFfp9-VhLcf73AzHQlyJQdVxdDrZM'
+        #self.ACCESS_TOKEN = ''
 
         # Keeping track of access token's expiration
         self.time_now = ''
@@ -29,6 +39,10 @@ class LongPoll:
 
         # Local directory for downloads
         self.local_directory = ''
+
+        # Array of detects images
+        self.images_detected = []
+
 
     # Timer
     def timer(self, timeout):
@@ -61,62 +75,37 @@ class LongPoll:
         else:
             return False
 
-    def get_cursor(self, path):
-        print("Fetching cursor")
-        cursor, has_more = lp.list_folder(path)
+    def latest_cursor(self, path):
 
-        while has_more:
-            cursor, has_more = lp.list_folder_continue(cursor)
+        # Define the URL and the access token
+        url = "https://api.dropboxapi.com/2/files/list_folder/get_latest_cursor"
 
-        print("Cursor fetched")
-        return cursor
-
-    def list_folder(self, path):
-        url = "https://api.dropboxapi.com/2/files/list_folder"
-
-        # Replace 'YOUR_ACCESS_TOKEN' with your actual OAuth access token
+        # Define the headers
         headers = {
-            "Authorization": f'Bearer {self.ACCESS_TOKEN}',  # OAuth Access Token
-            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.ACCESS_TOKEN}",
+            "Content-Type": "application/json"
         }
 
-        # Data to be sent in the request body
+        # Define the data payload
         data = {
             "include_deleted": False,
             "include_has_explicit_shared_members": False,
             "include_media_info": False,
             "include_mounted_folders": True,
             "include_non_downloadable_files": True,
-            "path": path,  # Path to the folder
+            "path": path,
             "recursive": True
         }
 
         # Send the POST request
-        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response = requests.post(url, headers=headers, json=data)
 
-        # Check the response status and print it
+        # Check the response status and print the result
         if response.status_code == 200:
-            #print(response.json())
-            return response.json()['cursor'], response.json()['has_more']
+            print("Success:", response.json())
+            return response.json()['cursor']
         else:
             print("Error:", response.status_code, response.text)
-
-    def list_folder_continue(self, cursor):
-
-        url = "https://api.dropboxapi.com/2/files/list_folder/continue"
-        headers = {
-            "Authorization": f"Bearer {self.ACCESS_TOKEN}",  # Replace with actual authorization header
-            "Content-Type": "application/json"
-        }
-
-        data = {
-            "cursor": cursor  # Your cursor value
-        }
-
-        response = requests.post(url, headers=headers, data=json.dumps(data))
-
-        # Print response to see the result
-        return response.json()['cursor'], response.json()['has_more']
 
     def list_changes(self, cursor):
 
@@ -135,6 +124,7 @@ class LongPoll:
         # If reponse contains entries, see which are image uploads
         if response.json()['entries']:
             self.image_detect(response.json())
+            print(response.json())
 
     def image_detect(self, response):
         # Check if token is expired
@@ -142,17 +132,33 @@ class LongPoll:
             self.refresh_access_token()
 
         # Loop through the entries and select those where .tag is 'file'
-        file_entries = [entry for entry in response['entries'] if entry['.tag'] == 'file']
+        file_entries = [entry['path_display'] for entry in response['entries'] if entry['.tag'] == 'file']
 
         # Print the file entries
         for entry in file_entries:
-            if entry['.tag'] == 'file':
-                if '.' in entry['path_display']:
-                    extension = entry['path_display'].rsplit('.', 1)[1]
-                    if extension in self.image_extensions:
-                        print(f"- Detected new image upload: {entry['path_display']}")
-                        #self.download({entry['path_display']})
+            if '.' in entry:
+                extension = entry.rsplit('.', 1)[1]
+                if extension in self.image_extensions:
+                    print(f"- Detected new image upload: {entry}")
+                    print(entry)
+                    self.images_detected.append(entry)
+                    self.download(entry)
 
+    def get_unique_filename(self, directory, filename):
+        # Get the full path of the file
+        base_name, extension = os.path.splitext(filename)
+
+        # Start with the original filename
+        new_filename = filename
+        counter = 1
+
+        # Loop until the file does not exist in the directory
+        while os.path.exists(os.path.join(directory, new_filename)):
+            # If the file exists, append a counter to the base name
+            new_filename = f"{base_name}_{counter}{extension}"
+            counter += 1
+
+        return new_filename
 
     def download(self, dropbox_path):
         # Check if token is expired
@@ -184,6 +190,9 @@ class LongPoll:
             # Ensure the directory exists locally
             os.makedirs(os.path.dirname(download_path), exist_ok=True)
 
+            # Ensure unique file path
+            #download_path = self.get_unique_filename(download_path, dropbox_path)
+
             # Save the binary data to a file
             with open(download_path, "wb") as file:
                 file.write(binary_data)
@@ -192,6 +201,53 @@ class LongPoll:
         else:
             print(f"Failed to download file. Status code: {response.status_code}")
             print(response.text)
+
+    def email_notification(self, unexpected_quit=False):
+        time_stamp = datetime.datetime.now()
+
+        notification = sendnetworkmail.SendNetworkEmail()
+        notification.sender("library@tv.cuny.edu")
+        # notification.recipients(["library@tv.cuny.edu"])
+        notification.recipients(["aida.garrido@tv.cuny.edu"])
+
+        if not unexpected_quit:
+            notification.subject(f"Dropbox images digest: {time_stamp}")
+
+            # Initialize an HTML formatted string for key-value pairs
+            html_output = ""
+
+            for file in self.images_detected:
+                html_output += f"{file}<br>\n"
+
+            html_content = f"""
+                    <html>
+                      <body>
+                        <p>Hello, </p>
+                        <p>The following images were recently uploaded to dropbox:</p>
+                        {html_output}
+                        <p></p>
+                        Best,
+                        <br>Library Bot
+                      </body>
+                    </html>
+                    """
+
+            # Set notification content
+            notification.html_content(html_content)
+
+            # Send the notification
+            notification.send()
+        else:
+            notification.subject(f"ERROR: {os.path.basename(__file__)} unexpectedly quit")
+
+            html_content = f"""<p>{os.path.basename(__file__)}, the program which detects new dropbox image uploads unexpectedly quit.</p>"""
+
+            # Set notification content
+            notification.content(html_content)
+
+            # Send the notification
+            notification.send()
+
 
     def longpoll(self, cursor, timeout):
         # Set the Dropbox API endpoint and the headers
@@ -224,18 +280,18 @@ if __name__ == "__main__":
     lp.refresh_access_token()
 
     # Specify dropbox path, if blank this means the entire dropbox
-    path = ""
-
+    db_path = ""
+    
     # Get cursor
-    cursor = lp.get_cursor(path)
+    cursor = lp.latest_cursor(db_path)
 
     # Specify timeout
     timeout = 30
 
     # Specify local directory for downloads
     lp.local_directory = "/Users/aidagarrido/Desktop/DOWNLOAD_TEST"
-    #lp.local_directory = input("Local directory for download: ")
-    #while not os.path.isdir(local_directory):
+    # lp.local_directory = input("Local directory for download: ")
+    # while not os.path.isdir(local_directory):
     #    lp.local_directory = input("Directory does not exist. Try again: ")
 
     while True:
@@ -244,15 +300,19 @@ if __name__ == "__main__":
         changes = lp.longpoll(cursor, timeout)
         if changes:
             print("changes")
-            lp.timer(10)
+            lp.timer(30)
+            new_cursor = lp.latest_cursor(db_path)
             lp.list_changes(cursor)
-            cursor = lp.get_cursor(path)
+
+            if lp.images_detected:
+                lp.email_notification()
+
+            cursor = new_cursor
+            lp.images_detected = []
         else:
             print('no changes')
 
-
     ## Next, checksum verification
-    ## Send email if new downloads
     ## Send email if application quits
     ## Schedule
     ## Create icon?
