b0af3b38c7cced2400cbbbfa01c6f28905f4b245
agarrkoch <85997789+agarrkoch@users.noreply.github.com>
2025-09-22 11:53:45 -0400

    Update restructurepackage.py
    significantly rewrote to accommodate multithreaded progress tracking

--- restructurepackage.py
+++ restructurepackage.py
@@ -11,12 +11,22 @@ import time
 import datetime
 import threading
 import re
+import filetype
 
 class RestructurePackage:
     def __init__(self):
         self.FILES_DICT = []
         self.TRANSFER_OKAY = True
         self.TRANSFER_ERROR = []
+
+        self.FILES_TO_TRANSFER = []
+
+        self.PROG_FILES = 0
+        self.TOTAL_FILES = 0
+        self.PROG_BYTES = 0
+        self.TOTAL_BYTES = 0
+        self.CURRENT_PROCESS = ''
+
 
     # Checks if directory is a mounted volume
     def mounted_volume(self, directory):
@@ -29,6 +39,41 @@ class RestructurePackage:
         os.makedirs(output_directory + f'/{package}', exist_ok=True)
         os.makedirs(self.unique_directory_path(output_directory + f'/{package}' + f'/metadata/logs/{subfolder}'))
         os.makedirs(self.unique_directory_path(output_directory + f'/{package}' + f'/objects/{subfolder}'))
+
+    def get_directory_stats(self, dir, transfer_type, files_dict=None, checksum=None):
+        total_size = 0
+        total_files = 0
+
+        archive_nondict_file_bytes = 0
+
+        for dirpath, _, filenames in os.walk(dir):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+
+                if transfer_type == 'delivery' and self.file_type(fp) != 'objects':
+                    continue
+
+                if self.mac_system_metadata(fp) or os.path.getsize(fp) == 0:
+                    continue
+
+                try:
+                    total_size += os.path.getsize(fp)
+                    total_files += 1
+                    self.FILES_TO_TRANSFER.append(fp)
+
+                    if files_dict and all(f["dest"] != fp for f in files_dict):
+                        archive_nondict_file_bytes += os.path.getsize(fp)
+                except (OSError, FileNotFoundError):
+                    pass
+
+        if checksum and files_dict:
+            self.TOTAL_BYTES = (total_size * 2) + archive_nondict_file_bytes
+        elif checksum and not files_dict:
+            self.TOTAL_BYTES = total_size * 3
+        else:
+            self.TOTAL_BYTES = total_size
+
+        self. TOTAL_FILES = total_files
 
     # Checks if file is mac system metadata, e.g. .DS_STORE
     def mac_system_metadata(self, file):
@@ -75,45 +120,16 @@ class RestructurePackage:
             directory_path = directory_path.rsplit("_", 1)[0] + f"_{counter}"
         return directory_path
 
-    # Determines if string can be cast to int
-    def can_cast_to_float(self, s):
-        try:
-            float(s)
-            return True
-        except ValueError:
-            return False
-
     # Determines file type using FFMPEG to bifurcate audio/video from all other kinds of file types
     def file_type(self, file_path):
-
-        # Number of streams
-        command = ["ffprobe", "-loglevel", "quiet", file_path, "-show_entries", "format=nb_streams", "-of", "default=nw=1:nk=1"]
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-        nb_streams = process.stdout.readline()
-
-        # Stream duration
-        command = ["ffprobe", "-loglevel", "quiet", file_path, "-show_entries", "stream=duration", "-of", "default=nw=1:nk=1"]
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-        duration = process.stdout.readline()
-
-        print(nb_streams)
-        print(duration)
-        if not self.can_cast_to_float(nb_streams):
-            nb_streams = 0
-        else:
-            nb_streams = int(nb_streams)
-        if not self.can_cast_to_float(duration):
-            duration = 0
-        else:
-            duration = float(duration)
-
-        if nb_streams >= 1 and duration > 0:
+        if filetype.is_av(file_path):
             return ("objects")
         else:
             return ("metadata/logs")
 
     # Copies file from source to destination, returns error and success bool tuple
-    def copy_file(self, input_file_path, output_file_path):
+    def copy_file(self, input_file_path, output_file_path, buffer_size=1024 * 1024):
+        self.CURRENT_PROCESS = f'Transferring {os.path.basename(input_file_path)}'
         max_retries = 3
         retries = 0
         # Copy file from source to destination
@@ -122,27 +138,29 @@ class RestructurePackage:
                 if not os.path.exists(input_file_path):
                     raise FileNotFoundError(f"{input_file_path} does not exist.")
 
-                shutil.copy2(input_file_path, output_file_path)
+                with open(input_file_path, 'rb') as fsrc, open(output_file_path, 'wb') as fdst:
+                    while True:
+                        buf = fsrc.read(buffer_size)
+                        if not buf:
+                            break
+                        fdst.write(buf)
+                        self.PROG_BYTES += len(buf)
                 # If successful, break out of the retry loop
-                print(f"Successfully copied {input_file_path} to {output_file_path}.")
                 return None, True
 
             except Exception as e:
                 retries += 1
-                print(f"Attempt {retries} of {max_retries} failed: An error occurred while copying the file: {e}")
                 if retries >= max_retries:
-                    print(
-                        f"Failed to copy {input_file_path} after {max_retries} attempts. Moving to the next file.")
                     return e, False
                 else:
                     time.sleep(2)  # Wait before retrying
 
     # Creates checksum, as per dropbox
     def calculate_sha256_checksum(self, file_path, block_size=4 * 1024 * 1024):
+        self.CURRENT_PROCESS = f'Calculating checksum for {os.path.basename(file_path)}'
+
         # Get the total size of the file
-        total_size = os.path.getsize(file_path)
         hash_object = hashlib.sha256()
-        bytes_read = 0
         block_hashes = []
 
         # Open the file for reading in binary mode
@@ -158,24 +176,19 @@ class RestructurePackage:
                 block_hashes.append(block_hash)
 
                 # Update the total bytes read
-                bytes_read += len(block)
-
+                self.PROG_BYTES += len(block)
                 # Update the main hash with the current block hash
                 hash_object.update(block_hash)
-
-                # Calculate and display progress
-                progress = min(int((bytes_read / total_size) * 100), 100)
-                sys.stdout.write("\rChecksum progress: [{:<50}] {:d}% ".format('=' * (progress // 2), progress))
-                sys.stdout.flush()
 
         # Compute the final hash of the concatenated block hashes
         final_hash = hash_object.hexdigest()
 
-        print()  # Move to the next line after progress
+        # Move to the next line after progress
         return final_hash
 
     # uses copy_file function to perform file transfer as per archive standards, e.g. checksum, file dict, error log
     def transfer_file(self, input_file_path, output_file_path, do_fixity, cs1=None):
+
         # Check if file path is unique, otherwise appends counter
         output_file_path = self.unique_file_path(output_file_path)
 
@@ -217,14 +230,11 @@ class RestructurePackage:
                 cs2 = self.calculate_sha256_checksum(output_file_path)
                 file_dict['checksum_d'] = cs2
                 if cs1 == cs2:
-                    print(f'File {input_file_path} transferred and passed fixity check')
                     file_dict["fixity_pass"] = True
                     self.FILES_DICT.append(file_dict)
                     return  # Exit on succesful copy
                 else:
                     if retries >= max_retries:
-                        print(
-                            f"Failed to transfer {input_file_path} after {max_retries} attempts. Moving to the next file.")
                         file_dict["fixity_pass"] = False
                         self.FILES_DICT.append(file_dict)
 
@@ -236,7 +246,6 @@ class RestructurePackage:
                         }
                         return  # Exit on max retries
 
-                    print(f'File {input_file_path} transferred but did not pass fixity check. Retrying file transfer.')
                     retries += 1
                     os.remove(output_file_path)
 
@@ -255,10 +264,10 @@ class RestructurePackage:
             else:
                 return  # No fixity check, exit loop
 
-    def archive_output_path(self, output_directory, output_package_name, foldername, output_subfolder_name, file):
+    def archive_output_path(self, output_directory, output_package_name, output_subfolder_name, file):
         output_file_path = os.path.join(output_directory, output_package_name,
-                                        self.file_type(os.path.join(foldername, file)),
-                                        output_subfolder_name, file)
+                                        self.file_type(file),
+                                        output_subfolder_name, os.path.basename(file))
         os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
         output_file_path = self.unique_file_path(output_file_path)
         return output_file_path
@@ -275,45 +284,38 @@ class RestructurePackage:
         output_file_path = self.unique_file_path(output_file_path)
         return output_file_path
 
-
     # core function, as of now, three copy types: archive, delivery, and one2one
     def restructure_copy(self, copy_type, input_folder_path, output_directory, output_package_name=None, output_subfolder_name=None,
                            do_fixity=None, do_delete=None, files_dict=None):
-        for foldername, subfolders, filenames in os.walk(input_folder_path, topdown=False):
-            for file in filenames:
-                if self.mac_system_metadata(file) or os.path.getsize(os.path.join(foldername, file)) == 0:
-                    try:
-                        os.remove(os.path.join(foldername, file))
-                    except Exception as e:
-                        print(f"Could not remove file {file}: {e}")     
-                else:
-                    print(f'Transferring file {file} to {output_directory}')
-                    # Initialize variables
-                    input_file_path = os.path.join(foldername, file)
-                    output_file_path = None
-                    cs1 = None
 
-                    if copy_type == "archive":
-                        output_file_path = self.archive_output_path(output_directory, output_package_name, foldername, output_subfolder_name, file)
-                    if copy_type == "delivery":
-                        output_file_path = self.delivery_output_path(output_directory, output_package_name, file)
-                    if copy_type == "one2one":
-                        output_file_path = self.one2one_output_path(output_directory, input_file_path, input_folder_path)
+        self.get_directory_stats(input_folder_path, copy_type, files_dict, do_fixity)
 
-                    if files_dict:
-                        for f in files_dict:
-                            if f['dest'] == input_file_path:  # Check the destination path
-                                cs1 = f['checksum_d']
-                                break
+        for file in self.FILES_TO_TRANSFER:
+            # Initialize variables
+            output_file_path = None
+            cs1 = None
 
-                    self.transfer_file(input_file_path, output_file_path, do_fixity, cs1)
+            if copy_type == "archive":
+                output_file_path = self.archive_output_path(output_directory, output_package_name, output_subfolder_name, file)
+            if copy_type == "delivery":
+                output_file_path = self.delivery_output_path(output_directory, output_package_name, os.path.basename(file))
+            if copy_type == "one2one":
+                output_file_path = self.one2one_output_path(output_directory, file, input_folder_path)
 
-                    if do_delete:
-                        os.remove(output_file_path)
+            if files_dict:
+                for f in files_dict:
+                    if f['dest'] == file:  # Check the destination path
+                        cs1 = f['checksum_d']
+                        break
 
-                if do_delete and not self.mounted_volume(foldername) and self.empty_folder(foldername):
-                    os.rmdir(foldername)
+            self.PROG_FILES = self.PROG_FILES + 1
+            self.transfer_file(file, output_file_path, do_fixity, cs1)
 
+            if do_delete:
+                os.remove(output_file_path)
+
+            if do_delete and not self.mounted_volume(os.path.dirname(file)) and self.empty_folder(os.path.dirname(file)):
+                os.rmdir(os.path.dirname(file))
 
 if __name__ == "__main__":
     # Define variables
@@ -381,5 +383,3 @@ if __name__ == "__main__":
         ingestcommands.makewindow(os.path.join(output_directory, package_name))
         ingestcommands.makemetadata(os.path.join(output_directory, package_name))
         ingestcommands.makechecksumpackage(os.path.join(output_directory, package_name))
-
-    #print error log in output directory
