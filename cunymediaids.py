650c93539142479bd48712a610eaab1c3a8d42cd
agarrkoch <85997789+agarrkoch@users.noreply.github.com>
2025-11-17 14:39:49 -0500

    Update cunymediaids.py
    moved dict to top

--- cunymediaids.py
+++ cunymediaids.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
 from difflib import SequenceMatcher
 
+# Create a dictionary from the provided list
+shows = {
+    "A Slice of NY":              "SLNY",
+    "Arts in the City":           "AITC",
+    "Asian American Life":         "AALF",
+    "Black America":               "BLAM",
+    "Bob Herbert’s OP-ED":        "OPED",
+    "Book It":                     "BKIT",
+    "Cafe con Felo":               "CFFL",
+    "City Cinematheque":           "CCT",
+    "City Works":                  "CTWR",
+    "Conversations with Jim Zirin": "CNJZ",
+    "CUNY Forum":                  "CFOR",
+    "CUNY TV Presents":       "CNTVPR",
+    "CUNY Uncut":                  "CNNT",
+    "CUNY Laureates":              "CNLR",
+    "CUNY Specials":               "SPEC",
+    "EdCast":                      "EDCA",
+    "Frame By Frame":               "FRBF",
+    "Graduate Center Presents":    "GCPR",
+    "Italics":                     "ITAL",
+    "Keeping Relevant":            "KPRL",
+    "LATiNAS":                     "LTNS",
+    "Laura Flanders Show":         "LFFR",
+    "Let It Rip":                  "LTRP",
+    "New York Times Close Up":     "NYTCU",
+    "Nueva York":                  "NUEV",
+    "One to One":                  "OTOO",
+    "Shades of US":                "SHUS",
+    "Sustainability Matters":       "STMT",
+    "Theater All the Moving Parts": "ATMP",
+    "Urban U":                     "URBN",
+    "Special":                      "SPEC"
+}
+
 def print_media_dict():
     # Print the titles and their abbreviations
     print(f"Media ID Dictionary")  # Header
@@ -56,37 +91,3 @@ def shows_string_contains(s):
 
     return matches
 
-# Create a dictionary from the provided list
-shows = {
-    "A Slice of NY":              "SLNY",
-    "Arts in the City":           "AITC",
-    "Asian American Life":         "AALF",
-    "Black America":               "BLAM",
-    "Bob Herbert’s OP-ED":        "OPED",
-    "Book It":                     "BKIT",
-    "Cafe con Felo":               "CFFL",
-    "City Cinematheque":           "CCT",
-    "City Works":                  "CTWR",
-    "Conversations with Jim Zirin": "CNJZ",
-    "CUNY Forum":                  "CFOR",
-    "CUNY TV Presents":       "CNTVPR",
-    "CUNY Uncut":                  "CNNT",
-    "CUNY Laureates":              "CNLR",
-    "CUNY Specials":               "SPEC",
-    "EdCast":                      "EDCA",
-    "Frame By Frame":               "FRBF",
-    "Graduate Center Presents":    "GCPR",
-    "Italics":                     "ITAL",
-    "Keeping Relevant":            "KPRL",
-    "LATiNAS":                     "LTNS",
-    "Laura Flanders Show":         "LFFR",
-    "Let It Rip":                  "LTRP",
-    "New York Times Close Up":     "NYTCU",
-    "Nueva York":                  "NUEV",
-    "One to One":                  "OTOO",
-    "Shades of US":                "SHUS",
-    "Sustainability Matters":       "STMT",
-    "Theater All the Moving Parts": "ATMP",
-    "Urban U":                     "URBN",
-    "Special":                      "SPEC"
-}
