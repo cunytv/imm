2f66c6f2f42be6457259ed944d2d786f9d9a4837
agarrkoch <85997789+agarrkoch@users.noreply.github.com>
2026-02-02 11:22:24 -0500

    Update validateuserinput.py
    updated package name validation logic

--- validateuserinput.py
+++ validateuserinput.py
@@ -7,33 +7,43 @@ from datetime import datetime
 
 
 def is_valid_package_name(s):
-    pattern = r'^[A-Za-z]+\d{4}(0[1-9]|1[0-2])(0[1-9]|[1-2]\d|3[0-1])_[A-Za-z0-9_]+$'
+    #pattern = r'^[A-Za-z]+\d{4}(0[1-9]|1[0-2])(0[1-9]|[1-2]\d|3[0-1])_[A-Za-z0-9_]+$'
 
-    if not re.match(pattern, s):
-        warning = f"{s} does not match show code and date pattern. Package name must include SHOWCODEYYYMMDD_DESCRIPTION. "
-        return False, warning
-    else:
-        bool = True
-        warning = ""
-        show_code = re.match(r"[A-Za-z]+", s).group() if re.match(r"[A-Za-z]+", s) else None
-        date = re.match(r".*?(\d+)_", s).group(1) if re.match(r".*?(\d+)_", s) else None
-        today_str = datetime.now().strftime("%Y%m%d")
+    #if not re.match(pattern, s):
+    #    warning = f"{s} does not match show code and date pattern. Package name must include SHOWCODEYYYMMDD_DESCRIPTION. "
+    #    return False, warning
+    
+    bool = True
+    warning = ""
+    show_code = re.match(r"[A-Za-z]+", s).group() if re.match(r"[A-Za-z]+", s) else None
+    date = re.match(r".*?(\d+)_", s).group(1) if re.match(r".*?(\d+)_", s) else None
+    today_str = datetime.now().strftime("%Y%m%d")
 
-        if not cunymediaids.is_code_in_dict(show_code):
-            warning += f"Show code {show_code} is not in dictionary. "
-            bool = False
-        if date != today_str:
-            warning += f"Date {date} is not today's date {today_str}. "
-            bool = False
+    if not show_code:
+        warning += f"Show code is missing. "
+        bool = False
+    elif not cunymediaids.is_code_in_dict(show_code):
+        warning += f"Show code {show_code} is not in dictionary. "
+        bool = False
 
-        return bool, warning
+    if not date:
+        warning += f"Date is missing. "
+        bool = False
+    elif date != today_str:
+        warning += f"Date {date} is not today's date {today_str}. "
+        bool = False
+
+    return bool, warning
 
 # Validates camera card package name by confirming show code and date pattern, capitalizing all letters, deleting spaces
 def card_package_name(name):
     valid_name, error = is_valid_package_name(name)
     while not valid_name:
         name = input(f"\033[31mWarning. {error}Press enter to continue or type new package name: \033[0m")
-        valid_name, error = is_valid_package_name(name)
+        if not name:
+            continue
+        else:
+            valid_name, error = is_valid_package_name(name)
 
     # Eliminates spaces and capitalizes letters
     name = name.replace(" ", "")
